dd07edf63304827f6ccf40a7978871c828de03a2
ChristianTC <christiantambo15@gmail.com>
2023-02-02 00:24:26 -0400

    graphics in Angular

--- src/app/pages/graphic1/graphic1.component.ts
+++ src/app/pages/graphic1/graphic1.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ChartData, ChartEvent, ChartType, Color } from 'chart.js';
 
 @Component({
   selector: 'app-graphic1',
@@ -7,5 +8,26 @@ import { Component } from '@angular/core';
   ]
 })
 export class Graphic1Component {
+  // Doughnut
+  public doughnutChartLabels: string[] = ['Download Sales', 'In-Store Sales', 'Mail-Order Sales'];
+  public doughnutChartData: ChartData<'doughnut'> = {
+    labels: this.doughnutChartLabels,
+    datasets: [
+      { 
+        data: [350, 450, 100],
+        backgroundColor: ['#6857E6', '#009FEE', '#F02059']
+      },
+    ]
+  };
+  public doughnutChartType: ChartType = 'doughnut';
+
+  // events
+  public chartClicked({ event, active }: { event: ChartEvent, active: {}[] }): void {
+    console.log(event, active);
+  }
+
+  public chartHovered({ event, active }: { event: ChartEvent, active: {}[] }): void {
+    console.log(event, active);
+  }
 
 }
--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AppRoutingModule } from '../app-routing.module';
 import { FormsModule } from '@angular/forms'
+import { NgChartsModule } from 'ng2-charts';
 
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { ProgressComponent } from './progress/progress.component';
@@ -31,6 +32,7 @@ import { ComponentsModule } from "../components/components.module";
         AppRoutingModule,
         FormsModule,
         ComponentsModule,
+        NgChartsModule,
     ]
 })
 export class PagesModule { }
