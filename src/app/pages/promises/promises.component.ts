0caf3d5e162ef00ae58076cafd89c8e27e95c1c1
ChristianTC <christiantambo15@gmail.com>
2023-02-06 19:09:28 -0400

    functions that return promises

--- src/app/pages/promises/promises.component.ts
+++ src/app/pages/promises/promises.component.ts
@@ -9,24 +9,35 @@ import { Component } from '@angular/core';
 export class PromisesComponent {
 
   ngOnInit(): void {
-    
-    const promise = new Promise( (resolve, reject) => {
-      if (false) {
-        resolve('hello world');
-      }else{
-        reject('Something no works')
+
+    this.getUsers().then(users => 
+      console.log(users)
+    )
+
+    // const promise = new Promise((resolve, reject) => {
+    //   if (false) {
+    //     resolve('hello world');
+    //   } else {
+    //     reject('Something no works')
+    //   }
+    // })
+
+    // promise.then((message) => {
+    //   console.log(message);
+    // }).catch((message) => {
+    //   console.log(message);
+    // })
+
+    // console.log('end');
+  }
+
+  getUsers() {
+    return new Promise( resolve => {
+      fetch('https://reqres.in/api/users')
+        .then(response => response.json())
+        .then(body => resolve(body.data))
       }
-    })
-
-    promise.then((message) => {
-      console.log(message);
-    }).catch((message) => {
-      console.log(message);
-    })
-
-    console.log('end');
-    
-
+    )
   }
 
 }
