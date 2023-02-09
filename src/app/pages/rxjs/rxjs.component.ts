1233b1c297cc2c936524cb4ed49bc60e307b5518
ChristianTC <christiantambo15@gmail.com>
2023-02-08 20:26:49 -0400

    Observable: add method Retry

--- src/app/pages/rxjs/rxjs.component.ts
+++ src/app/pages/rxjs/rxjs.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { Observable } from 'rxjs';
+import { retry } from 'rxjs/operators';
 
 @Component({
   selector: 'app-rxjs',
@@ -10,23 +11,26 @@ import { Observable } from 'rxjs';
 export class RxjsComponent {
 
   constructor() {
-
+    
     const obs$ = new Observable( observer => {
       let num = 0
       const interval = setInterval(()=>{
+        num++
         observer.next(num)
         if (num === 5) {
           observer.complete
           clearInterval(interval)
         }
-        if (num === 7) {
+        if (num === 2) {
+          num = 0
           observer.error('Error')
         }
-        num++
       },1000)
     })
 
-    obs$.subscribe({
+    obs$.pipe(
+      retry(1)
+    ).subscribe({
       next: value => console.log('Subs', value),
       error: (error) => console.error('Error', error),
       complete: () => console.log('Obs completed')
